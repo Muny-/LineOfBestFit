ee679b5c0188a0991e2db0a918db164a8a09b900
Kevin Smith <kevin@muny.us>
2017-12-07 01:16:45 -0500

    Prettification and fixed logic

--- Program.cs
+++ Program.cs
@@ -23,10 +23,10 @@ namespace LineOfBestFit
         {
             Console.Write("Loading input vectors...");
 
-            // Read all of InputDataPoints.txt into an array of strings,
-            // where the delimiter is a \n character, then convert
-            // that into a list of DataPoint objects (x, y) 
-            List<DataPoint> data_points = File.ReadAllLines("InputDataPoints.txt")
+            // Read all of InputDataPoints.txt (or first program argument)
+            // into an array of strings, where the delimiter is a \n
+            // character, then convert that into a list of DataPoint objects (x, y) 
+            List<DataPoint> data_points = File.ReadAllLines(args.Length > 0 ? args[0]: "InputDataPoints.txt")
                 .Where(val => !val.StartsWith("#")).ToList().ConvertAll(val => new DataPoint(ParseVector(val, 25, 13))).ToList();
 
             Console.WriteLine("done");
@@ -45,20 +45,33 @@ namespace LineOfBestFit
             data_points.ForEach(val => val.SetAvgs(X_Avg, Y_Avg));
 
             // Find the sum of all of each item's (XAvgDist*YAvgDist) value
+            // This is justified in the write-up
             double sum_distance_products = data_points.Sum(val => val.DistanceFromX_Avg * val.DistanceFromY_Avg);
 
             // Find the sum of all of each item's (XAvgDist^2) value
+            // Or, in other words, find the sum of all of the errors^2
+            // We square the errors here for the following reasons:
+            //   We don't want negatives (abs sounds like a good option, but...)
+            //   We want dval/derr to not be proportional, exponential instead; plus
+            //      abs values are ugly
             double sum_xavg_dist_squares = data_points.Sum(val => val.DistanceFromX_Avg * val.DistanceFromX_Avg);
 
             // Find the slope of the best fit line by dividing the sum of the distance products by the x dist squares.
+            // Justified in the write-up
             double best_fit_line_slope = sum_distance_products / sum_xavg_dist_squares;
 
             // Find the y intercept of the line of best fit
+            // Justified in the write-up
             double y_intercept = Y_Avg - (best_fit_line_slope * X_Avg);
+
+            // So finally the equation looks like:
+            // 
+            //       y = best_fit_line_slope*x + y_intercept
+            //
 
             Console.WriteLine("Equation: y = " + best_fit_line_slope + "x + " + y_intercept);
 
-            // Write plot visualization files
+            // [Housekeeping...] Write plot visualization files
 
             StringBuilder plot_data = new StringBuilder();
 
@@ -92,16 +105,17 @@ pause -1 'press Ctrl-D to exit'";
             // Write output plot to file
             File.WriteAllText("OutputPlot.gnu", gnuplot);
 
+            // Display our results!
             "gnuplot OutputPlot.gnu".Bash();
         }
 
-        // Parses a string that looks like "int,int,int,..." into a Vector2d object using the specified indexes for x and y values
+        // Parses a string that looks like "int,int,int,..." into a Vector2d object using the specified indices for x and y values
         static Vector2d ParseVector(string str, int x_index, int y_index)
         {            
             string[] parts = str.Trim().Split(",");
 
             if (parts.Length < 2)
-                throw new FormatException("Not enough characteristics");
+                throw new FormatException("Too few characteristics, need at least 2");
 
             double x;
 
@@ -117,12 +131,15 @@ pause -1 'press Ctrl-D to exit'";
         }
     }
 
+    // Helper class to give us an easy data structure to work with
     public class DataPoint
     {
         private double _x_avg, _y_avg;
 
+        // Coordinates in cartesian plane
         public Vector2d Location;
 
+        // Horizontal distance from average x value of data
         public double DistanceFromX_Avg
         {
             get
@@ -131,6 +148,7 @@ pause -1 'press Ctrl-D to exit'";
             }
         }
 
+        // Vertical distance from average y value of data
         public double DistanceFromY_Avg
         {
             get
@@ -139,16 +157,19 @@ pause -1 'press Ctrl-D to exit'";
             }
         }
 
+        // Class constructor
         public DataPoint(Vector2d location)
         {
             this.Location = location;
         }
 
+        // Housekeeping...
         public override string ToString()
         {
             return Location.ToString();
         }
 
+        // Housekeeping...
         public void SetAvgs(double x_avg, double y_avg)
         {
             _x_avg = x_avg;
